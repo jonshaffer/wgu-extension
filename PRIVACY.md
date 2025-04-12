cf782626e0bf3229187f02c76079481e63f71fea
Jonathan Shaffer <jon@hyperfluidsolutions.com>
2025-04-12 12:30:45 -0400

    chore: initial release

---
+++ PRIVACY.md
@@ -0,0 +1,31 @@
+# Privacy Policy
+
+**Effective Date:** 12 April 2025
+
+WGU Extension is committed to protecting your privacy. This Privacy Policy explains how we handle your information when you use our app.
+
+## 1. No Data Collection
+
+We do not collect, store, or process any personal data from users. The app functions entirely on your device without transmitting any information to external servers.
+
+## 2. No Third-Party Services
+
+Our app does not use third-party analytics, advertisements, or tracking services. Your usage remains private and is not shared with any external entities.
+
+## 3. Local Data Storage
+
+Any data you enter into the app is stored locally on your device. We do not have access to this data, nor do we back it up to any external servers.
+
+## 4. Security
+
+Since no data is collected or transmitted, there are no security risks associated with sharing information through the app. However, we recommend that you take appropriate measures to secure your device.
+
+## 5. Changes to This Privacy Policy
+
+If any changes are made to this policy, we will update this document accordingly. Continued use of the app after changes are made indicates your acceptance of the updated policy.
+
+## 6. Contact Us
+
+If you have any questions about this Privacy Policy, you can contact us at [jon@hyperfluidsolutions.com](mailto:jon@hyperfluidsolutions.com).
+
+By using WGU Extension, you agree to this Privacy Policy.
---
+++ entrypoints/content.ts
@@ -0,0 +1,60 @@
+import { storage } from "@wxt-dev/storage";
+import { SHOW_REPORT_PERCENTAGE } from "@/utils/storage.constants";
+
+export default defineContentScript({
+  // matches like https://my.wgu.edu/coaching-report/
+  matches: ['https://my.wgu.edu/coaching-report/*'],
+  runAt: 'document_end',
+
+  registration: "manifest",
+
+  main(_ctx) {
+    const checkIfVisible = () => {
+      const assessment = document.querySelector('cr-assessment');
+      if (assessment) {
+        const style = window.getComputedStyle(assessment);
+        if (style.display !== 'none' && style.visibility !== 'hidden') {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    const interval = setInterval(() => {
+      storage.getItem<boolean>(SHOW_REPORT_PERCENTAGE).then((val) => {
+        if (!val) return;
+        
+        if (checkIfVisible()) {
+          clearInterval(interval);
+          labelProgressBars();
+        }
+      })
+    }, 1000);
+
+    function labelProgressBars() {
+      const progressBar = document.querySelectorAll('.cr-progress-bar');
+
+      for (let i = 0; i < progressBar.length; i++) {
+        const lpb = progressBar[i];
+        const progressBarValue = lpb.querySelector('.progress .value');
+
+        if (progressBarValue) {
+          // @ts-expect-error
+          const right = progressBarValue.style.right;
+          const percentage = 100 - parseFloat(right);
+          const label = lpb.parentElement?.parentElement?.querySelector('.cr-progress-bar-label span') || lpb.parentElement?.parentElement?.parentElement?.querySelector('.cr-assessment-score-label');
+          
+          if (label) {
+            const span = document.createElement('span');
+            span.className = 'wgu-ext-percentage';
+            span.dataset.source = 'wgu-extension';
+            span.textContent = ` (${percentage}%)`;
+
+            label.appendChild(span);
+          }
+        }
+      }
+    }
+
+  },
+});
