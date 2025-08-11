beeab60917306696b03f4d0cffe545e20204ce1c
Jon Shaffer <properjon@gmail.com>
2025-08-11 10:15:26 -0400

    types(courses): add Zod schemas and exports in shared package (@wgu-extension/types)

--- extension/packages/types/src/index.ts
+++ extension/packages/types/src/index.ts
@@ -1,4 +1,5 @@
 export * from './reddit';
 export * from './discord';
 export * from './catalog';
+export * from './courses';
 // Note: canonical raw types are not re-exported here to avoid rootDir issues.
